4b88956c2b354af544ede101e7277188151c77f2
alselnet <alex.selnet@gmail.com>
2024-01-02 13:15:51 +0100

    CPP07/ex01 protected void function input in iter

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: aselnet <aselnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:36:16 by aselnet           #+#    #+#             */
-/*   Updated: 2023/12/17 09:46:42 by aselnet          ###   ########.fr       */
+/*   Updated: 2024/01/02 13:12:54 by aselnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,25 @@
 #include <iostream>
 
 template <typename T>
-void	iter(T *arr_ptr, int arr_len, void (f)(T *))
+void	iter(T *arr_ptr, int arr_len, void (&f)(T const &))
 {
-	int	i = 0;
-	T	*ptr = arr_ptr;
-	if (arr_ptr == NULL || arr_len == 0)
-		return ;
-	while (i < arr_len)
+	int	i;
+	if (arr_len < 0)
 	{
-		f (ptr);
-		ptr++;
-		i++;
+		std::cerr << "wrong size" << std::endl;
+		return ;
 	}
+	i = -1;
+	while (++i < arr_len)
+		f(arr_ptr[i]);
+
 	return ;
 }
 
 template <typename T>
-void	print_elem(T *elem)
+void	print_elem(T const &elem)
 {
-	std::cout << *elem << std::endl;
+	std::cout << elem << std::endl;
 	return ;
 }
 
